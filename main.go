3fdb28d36c2a070842f5ae04043d1dcaae87cd0a
WBetEngine <wbetengine@gmail.com>
2025-04-11 00:40:52 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"whitelabel/internal/customer/config"
+	"whitelabel/internal/customer/routers"
+)
+
+func main() {
+	// Inisialisasi aplikasi customer
+	log.Println("Menginisialisasi aplikasi...")
+	config.Init()
+	
+	// Set up file server for static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Setup customer routes
+	routers.SetupRoutes()
+
+	// Start the server
+	log.Println("Server starting on http://localhost:8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
